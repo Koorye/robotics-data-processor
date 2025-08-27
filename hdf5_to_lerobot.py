8227af75bf779caa0e1f217a66ea81319f9c030a
Koorye <a1311586225@gmail.com>
2025-08-27 17:03:30 +0800

    update

--- hdf5_to_lerobot.py
+++ hdf5_to_lerobot.py
@@ -34,10 +34,15 @@ import imageio
 import io
 import numpy as np
 import os
+import shutil
 from PIL import Image
 from tqdm import tqdm
 
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
+
+
+def get_lerobot_root():
+    return os.path.join(os.path.expanduser('~'), '.cache', 'huggingface', 'lerobot')
 
 
 def decode_image(image_buffer):
@@ -107,6 +112,48 @@ def find_hdf5_paths(root):
     return hdf5_paths
 
 
+def hdf5_to_lerobot(hdf5_paths, dataset):
+    for ep, path in enumerate(hdf5_paths):
+        frames = load_hdf5(path)
+        for frame in tqdm(frames, desc=f'Processing episode {ep} {path}'):
+            dataset.add_frame(frame, task='catch banana into basket')
+        dataset.save_episode()
+
+
+def main(args):
+    save_root = os.path.join(get_lerobot_root(), args.repo_id)
+    if os.path.exists(save_root):
+        if not args.overwrite:
+            print(f'Dataset {args.repo_id} already exists. Use --overwrite to overwrite.')
+            return
+        else:
+            print(f'Overwriting existing dataset {args.repo_id}.')
+            shutil.rmtree(save_root)
+
+    hdf5_paths = find_hdf5_paths(args.root)
+
+    if not hdf5_paths:
+        print(f'No hdf5 files found in {args.root}')
+        return
+    
+    if args.max_episodes > 0:
+        hdf5_paths = hdf5_paths[:args.max_episodes]
+    
+    print('HDF5 files to process:')
+    for path in hdf5_paths:
+        print(' -', path)
+    
+    dataset = create_lerobot_dataset(
+        repo_id=args.repo_id,
+        example_data=load_hdf5(hdf5_paths[0])[0],
+        fps=args.fps,
+        video_backend=args.video_backend,
+        image_writer_threads=args.image_writer_threads,
+        image_writer_processes=args.image_writer_processes,
+    )
+    hdf5_to_lerobot(hdf5_paths, dataset)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', type=str, required=True, help='Input directory containing hdf5 files')
@@ -116,31 +163,6 @@ if __name__ == '__main__':
     parser.add_argument('--image_writer_threads', type=int, default=10, help='Number of threads for image writing')
     parser.add_argument('--image_writer_processes', type=int, default=5, help='Number of processes for image writing')
     parser.add_argument('--max_episodes', type=int, default=-1, help='Maximum number of episodes to process (-1 for all)')
+    parser.add_argument('--overwrite', action='store_true', help='Overwrite existing dataset if it exists')
     args = parser.parse_args()
-
-    hdf5_paths = find_hdf5_paths(args.root)
-    if not hdf5_paths:
-        print(f'No hdf5 files found in {args.root}')
-        exit(1)
-    if args.max_episodes > 0:
-        hdf5_paths = hdf5_paths[:args.max_episodes]
-    
-    print('HDF5 files to process:')
-    for path in hdf5_paths:
-        print(' -', path)
-
-    example_data = load_hdf5(hdf5_paths[0])[0]
-    dataset = create_lerobot_dataset(
-        repo_id=args.repo_id, 
-        example_data=example_data,
-        fps=args.fps,
-        video_backend=args.video_backend,
-        image_writer_threads=args.image_writer_threads,
-        image_writer_processes=args.image_writer_processes,
-    )
-
-    for ep, path in enumerate(hdf5_paths):
-        frames = load_hdf5(path)
-        for frame in tqdm(frames, desc=f'Processing episode {ep} {path}'):
-            dataset.add_frame(frame, task='catch banana into basket')
-        dataset.save_episode()
+    main(args)
